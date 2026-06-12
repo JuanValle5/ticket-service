63d7bc533b70392326a712d21531e536255dd73f
JuanValencia <juan.j.valencia@correounivalle.edu.co>
2026-06-12 06:58:54 -0500

    reitentos en enviar emails, en caso de que salgan failed

--- src/main/java/com/vivaeventos/boletas_service/BoletasServiceApplication.java
+++ src/main/java/com/vivaeventos/boletas_service/BoletasServiceApplication.java
@@ -2,12 +2,15 @@ package com.vivaeventos.boletas_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class BoletasServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoletasServiceApplication.class, args);
+        //amoo wtiq aoke udmt
     }
 
 }
--- src/main/java/com/vivaeventos/boletas_service/service/EmailService.java
+++ src/main/java/com/vivaeventos/boletas_service/service/EmailService.java
@@ -5,22 +5,47 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.support.RetrySynchronizationManager;
 import org.springframework.stereotype.Service;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.support.RetrySynchronizationManager;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Retryable(
+            retryFor = Exception.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 3000)
+    )
     public void sendEmailWithAttachments(
             String to,
             String subject,
             String body,
             Map<String, byte[]> attachments
     ) {
+
+        Integer retryCount =
+                RetrySynchronizationManager.getContext() != null
+                        ? RetrySynchronizationManager
+                        .getContext()
+                        .getRetryCount()
+                        : 0;
+
+        log.info(
+                "Intento de envío #{} para {}",
+                retryCount + 1,
+                to
+        );
 
         try {
 
@@ -50,7 +75,18 @@ public class EmailService {
 
             mailSender.send(mimeMessage);
 
+            log.info(
+                    "Correo enviado exitosamente a {}",
+                    to
+            );
+
         } catch (Exception e) {
+
+            log.error(
+                    "Error enviando correo a {}: {}",
+                    to,
+                    e.getMessage()
+            );
 
             throw new RuntimeException(
                     "Error enviando correo",
