1e8a485e309e871683be50a17521cdd06b25ff02
JuanValencia <juan.j.valencia@correounivalle.edu.co>
2026-06-12 05:23:37 -0500

    llamado para crear ticket cada que haya una order confirmada

--- src/main/java/com/vivaeventos/boletas_service/messaging/OrderConfirmedListener.java
+++ src/main/java/com/vivaeventos/boletas_service/messaging/OrderConfirmedListener.java
@@ -1,12 +1,18 @@
 package com.vivaeventos.boletas_service.messaging;
 
+import com.vivaeventos.boletas_service.dto.CreateTicketRequest;
+import com.vivaeventos.boletas_service.service.TicketService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class OrderConfirmedListener {
+
+    private final TicketService ticketService;
 
     @RabbitListener(
             queues = RabbitConstants.ORDER_CONFIRMED_QUEUE
@@ -19,5 +25,19 @@ public class OrderConfirmedListener {
                 "Orden confirmada recibida: {}",
                 message.getOrderId()
         );
+
+        message.getItems().forEach(item -> {
+
+            for (int i = 0; i < item.getQuantity(); i++) {
+
+                ticketService.createTicket(
+                        new CreateTicketRequest(
+                                message.getEventId(),
+                                message.getOrderId(),
+                                message.getBuyerId()
+                        )
+                );
+            }
+        });
     }
 }
